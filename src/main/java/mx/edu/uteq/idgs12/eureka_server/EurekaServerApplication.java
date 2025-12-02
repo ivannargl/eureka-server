04a86cb2da4418b0bcbc9cc28feecc71b35fed40
ivannargl <r.guzmanlopez89@gmail.com>
2025-12-01 19:47:51 -0600

    Eureka server para attendance

---
+++ src/main/java/mx/edu/uteq/idgs12/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package mx.edu.uteq.idgs12.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
